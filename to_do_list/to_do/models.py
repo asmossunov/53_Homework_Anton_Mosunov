92fb7f707433c0fae633940466f68304995a9ceb
Mosunov Anton <antonmossunov@gmail.com>
2022-09-19 11:43:50 +0600

    Модель, миграции, админка
    Создали модель Task
    Произвели миграции
    Настроили подключение к админке: superuser (admin, admin)

--- to_do_list/to_do/models.py
+++ to_do_list/to_do/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_text = models.TextField(verbose_name='Текст задачи', max_length=3000, null=False, blank=False)
+    state = models.CharField(verbose_name='Состояние', max_length=300, null=False, blank=False, default='new')
+    deadline = models.DateTimeField(verbose_name='Дедлайн')
+
+    def __str__(self):
+        return f'{self.task_text} {self.state} {self.deadline}'
