0f9937b4591b8f09806bb6d55f50e0b251829d0e
Mosunov Anton <antonmossunov@gmail.com>
2022-09-20 01:12:20 +0600

    Конфигурация параметров выбора.
    Добавлен файл choice_config.py для хранения параметров выбора состояния задачи
    Реформатирован шаблон принимающий в качестве контекста переменную с параметрами конфигуации
    Попутно отредактирован файл task.py из представлений.

--- to_do_list/to_do/views/tasks.py
+++ to_do_list/to_do/views/tasks.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from to_do.models import Task
 
+from to_do.choice_config import CHOICES
+
 
 def index_view(request):
     if request.method == 'GET':
@@ -15,13 +17,18 @@ def index_view(request):
 
 
 def add_task_view(request):
-    return render(request, 'add_task.html')
+    context = {
+        'CHOICES': CHOICES
+    }
+    return render(request, 'add_task.html', context)
 
 
 def added_task_prepare(request):
+    print(request.POST)
     task = Task.objects.create(
         task_text=request.POST.get("task_text"),
-        deadline=request.POST.get("deadline")
+        deadline=request.POST.get("deadline"),
+        state=request.POST.get("state")
     )
     tasks = Task.objects.all()
     context = {
