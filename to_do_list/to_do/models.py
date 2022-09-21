d70b9b17e5fad6759eb3c407044dd4c04844f811
Mosunov Anton <antonmossunov@gmail.com>
2022-09-22 00:25:42 +0600

    Редактирование модели и создание миграции.
    Добавили в модель поле описания
    Создали миграцию
    Применили миграцию

--- to_do_list/to_do/models.py
+++ to_do_list/to_do/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     task_text = models.TextField(verbose_name='Текст задачи', max_length=3000, null=False, blank=False)
+    task_description = models.TextField(verbose_name='Описание', max_length=3000, null=False, default='')
     state = models.CharField(verbose_name='Состояние', max_length=300, null=False, blank=False, default='new')
     deadline = models.DateTimeField(verbose_name='Дедлайн')
 
