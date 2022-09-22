57bd5ad69ae5aaf9c12dfd3751654ee3df474425
Mosunov Anton <antonmossunov@gmail.com>
2022-09-22 11:15:55 +0600

    Изменение параметров поля DateTimeField модели.
    Изменены параметры поля даты для реализации возможности отправки из формы null значения.
    Создана и произведена миграция.

--- to_do_list/to_do/models.py
+++ to_do_list/to_do/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     task_text = models.TextField(verbose_name='Текст задачи', max_length=3000, null=False, blank=False)
     task_description = models.TextField(verbose_name='Описание', max_length=3000, null=False, default='')
     state = models.CharField(verbose_name='Состояние', max_length=300, null=False, blank=False, default='new')
-    deadline = models.DateTimeField(verbose_name='Дедлайн')
+    deadline = models.DateTimeField(verbose_name='Дедлайн', null=True, default=None)
 
     def __str__(self):
         return f'{self.task_text} {self.state} {self.deadline}'
